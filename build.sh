57f0064368ed190c05544afeedee110a308c7ae4
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2026-06-13 22:32:13 +0100

    uploading

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python manage.py migrate
 # Create superuser only if environment variables are set and superuser doesn't exist
 # This command will attempt to create a superuser and will fail if one already exists,
 # but `|| true` will prevent the build from failing.
+if [[ $DJANGO_SUPERUSER_PASSWORD ]]; then
+  echo "Attempting to create superuser..."
+  python manage.py createsuperuser --no-input || true
+fi
